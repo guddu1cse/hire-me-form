379ecd58615efdc17e7bc6a8b0769813609d417a
Guddu Kumar <guddu.kumar@devxconsultancy.com>
2025-06-14 02:49:39 +0530

    let's connect with guddu

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/todoist/',
+  base: '/hire-me-form/',
   plugins: [react(), tailwindcss()],
 })
