77d90a581b351d74d06b898c952d22913bc16753
Guddu Kumar <guddu.kumar@devxconsultancy.com>
2025-06-23 15:45:17 +0530

    _track-visits

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 import HireMeForm from './component/HireMeForm'
 function App() {
-  const [count, setCount] = useState(0)
+
+  useEffect(()=>{
+    fetch(`${import.meta.env.VITE_BASE_URL}/api/track-visit`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({}), // empty object as body
+    })
+  } , []);
+  
   return <HireMeForm />
 }
 
